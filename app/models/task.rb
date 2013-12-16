8d0ec4883e5c612bafa0986fecde791d6e2690db
Graham Powrie <graham@asbp.io>
2013-12-16 10:00:43 -0500

    renamed todo to task for ember, added some start task specs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+  validates_presence_of :title, :message => "can't be blank"
+  validates_presence_of :priority, :message => "can't be blank"
+  validates :priority, inclusion: { in: %w( high medium low) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:task) { build :task, priority: nil }
+  it "requires a priority" do
+    expect(task).to_not be_valid
+  end
+  
+  let(:task) { build :task, priority: "highest" }
+  it "require a priority of only high/medium/low" do
+    expect(task).to_not be_valid
+  end
+  
+  
 end
